2d28a0b39b22f05db2175ffa0f7457a900aae20b
Nathanael Brian <22826533+NatBrian@users.noreply.github.com>
2023-01-29 13:31:18 +0700

    refactor api key

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import NewsList from "./components/NewsList";
 import axios from "axios";
 import DatePicker from "react-datepicker";
-import MY_API_KEY from "apikey.js"
 import "react-datepicker/dist/react-datepicker.css";
 
 const App = () => {
@@ -16,7 +15,7 @@ const App = () => {
 
   const API_URL_HEADLINES = "https://newsapi.org/v2/top-headlines";
   const API_URL_EVERYTHING = "https://newsapi.org/v2/everything";
-  const API_KEY = MY_API_KEY;
+  const API_KEY = process.env.REACT_APP_API_KEY;
 
   const handleSubmit = (event) => {
     event.preventDefault();
